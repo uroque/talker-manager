b73b7ae66f99b55c15a1b0e58edefb93e754cb9f
uroque <ulissesroque@protonmail.com>
2022-02-17 10:50:53 -0300

    feat: implements GET /talker

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { readFile } = require('fs/promises');
+
+const TALKER_LIST = './talker.json';
 
 const app = express();
 app.use(bodyParser.json());
@@ -10,6 +13,15 @@ const PORT = '3000';
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+app.get('/talker', async (_req, res, next) => {
+  try {
+    const talkerList = JSON.parse(await readFile(TALKER_LIST, 'utf-8'));
+    return res.status(HTTP_OK_STATUS).json(talkerList);
+  } catch (err) {
+    return next(err);
+  }
 });
 
 app.listen(PORT, () => {
